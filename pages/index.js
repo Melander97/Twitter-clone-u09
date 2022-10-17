d26a6d65636f168cdb67fb954535739c9da76386
melander97 <alexandermelander1@gmail.com>
2022-10-17 18:21:17 +0200

    create feed component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Sidebar from '../components/Sidebar'
 import styles from '../styles/Home.module.css'
+import Sidebar from '../components/Sidebar'
+import Feed from '../components/Feed'
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
     
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
          <Sidebar />      
-         {/* Feed */} 
+         <Feed/>
          {/* Widgets */}   
          
          {/* Modal */}   
